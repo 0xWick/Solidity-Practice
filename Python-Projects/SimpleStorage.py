e268ca488bb40f85adb5d4000975cdcffb060c80
Wick.py <wickprof@gmail.com>
2021-12-20 18:17:58 +0500

    Prettify Solidity in SimpleStorage.sol
    -> Formatted pretify in SimpleStorage.sol
    -> Added Python-Projects\SimpleStorage.py

---
+++ Python-Projects/SimpleStorage.py
@@ -0,0 +1,1 @@
+print('COME HERE BOI!')
--- Solidity-Projetcs/SimpleStorage.sol
+++ Solidity-Projetcs/SimpleStorage.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity 0.6.0;
+
 //Storage Program by Patrick, freecodecamp!
 contract SimpleStorage {
-
     // Initial value = 0
     uint256 favouriteNumber;
     uint256 add;
@@ -17,12 +17,12 @@ contract SimpleStorage {
     // View & Pure functions just read off the blockchain thus not making any state changes
 
     // pure functions are used for purely doing math, not sure how we gonna use it if we can't make state changes XD !!!
-    function addition(uint256 _add) public pure{
+    function addition(uint256 _add) public pure {
         _add + _add;
     }
 
     //view function to retrieve our value
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         return favouriteNumber;
     }
 
@@ -32,23 +32,22 @@ contract SimpleStorage {
         string name;
     }
     //Add values to struct
-    People public person = People({favNum : 69, name : "WickB"});
+    People public person = People({favNum: 69, name: "WickB"});
 
     // Lets add more than 1 persons using Arrays
 
     //Defining a Dynamic Array
     People[] public people;
 
-     // Finding Wick using Mapping
+    // Finding Wick using Mapping
     mapping(uint256 => string) public WickToNum;
 
     // function for appending users in People Lists
     function addPerson(string memory _name, uint256 _favNum) public {
-        people.push(People({favNum : _favNum, name: _name}));
-        people.push(People({favNum : 7, name : "WickJ"}));
+        people.push(People({favNum: _favNum, name: _name}));
+        people.push(People({favNum: 7, name: "WickJ"}));
 
         //Adding Mapping in here
         WickToNum[_favNum] = _name;
     }
-
 }
